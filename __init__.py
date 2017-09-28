a108194b6ef631d0acef0d9d30fd7557465d4b81
Ghostkeeper <rubend@tutanota.com>
2017-09-28 14:17:09 +0200

    Add plug-in metadata
    These are the two metadata files for a plug-in. One is loaded by the plug-in registry. The other is used to construct the plug-in objects.
    
    Made for internal ticket CURA-4347.

---
+++ __init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2017 Ultimaker B.V.
+# This example is released under the terms of the AGPLv3 or higher.
+
+from . import ExampleTool
+
+##  Defines additional metadata for the plug-in.
+#
+#   Tool plug-ins can have a button in the interface. This has some metadata
+#   that describes the tool and provides an image.
+def getMetaData():
+    return {
+        "tool": {
+            "name": "Example Tool",
+            "description": "This is an example tool to illustrate to plug-in developers how tool plug-ins are created.", #Typically displayed when hovering over the tool icon.
+            "icon": "wrench.svg" #Displayed on the button.
+        }
+    }
+
+##  Lets Uranium know that this plug-in exists.
+#
+#   This is called when starting the application to find out which plug-ins
+#   exist and what their types are. We need to return a dictionary mapping from
+#   strings representing plug-in types (in this case "tool") to objects that
+#   inherit from PluginObject.
+#
+#   \param app The application that the plug-in needs to register with.
+def register(app):
+    return {"tool": ExampleTool.ExampleTool()}
